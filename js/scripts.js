fb9243a3e8ae63add235326ec9d44d5455264a2b
Shaylee G <shaygracey@gmail.com>
2019-12-20 10:29:57 -0800

    replaced lightbox with fancybox

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $('#search').on('keyup', function(){
     const value = $('#search').val().toLowerCase();
 
     $('.photo-container a').each(function(index, element){
-       const captionAttr = $(this).attr('data-title').toLowerCase();
+       const captionAttr = $(this).attr('data-caption').toLowerCase();
 
         if (captionAttr.indexOf(value) > -1){
             $('this').show();
